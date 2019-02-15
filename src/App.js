7262343a299916c9f973c8fe67c8b3add5e30704
hurenjie <1596910974@qq.com>
2019-02-15 17:15:50 +0800

    项目搭建

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        haha哈哈
+      </div>
+    );
+  }
+}
+
+export default App;
