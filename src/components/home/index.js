5e3d658e2b82046eb0a948c372fb2384349aec6d
hurenjie <1596910974@qq.com>
2019-02-15 20:42:07 +0800

    hurenjie

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,7 +3,7 @@ export default class Home extends Component{
     render(){
         return(
             <div>
-                <h2>我是home</h2>
+                <h2>我是homeaa</h2>
             </div>
         )
     }
